f272236d70153affe4f3a2c6ff61677f8bedf4cf
Ivan Herak <herak.ivan@gmail.com>
2017-11-30 17:12:15 +0100

    Initial implementation of the multiupload

--- Controller/MenuPluginController.php
+++ Controller/MenuPluginController.php
@@ -5,6 +5,7 @@ namespace Netgen\Bundle\RemoteMediaBundle\Controller;
 use \Cloudinary;
 use \Cloudinary\Api;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 
@@ -121,6 +122,73 @@ class MenuPluginController extends Controller
                 'next_cursor' => isset($resources['next_cursor']) ? $resources['next_cursor'] : false
             ]
         );
+    }
 
+    private function handleFiles(array $files)
+    {
+        $languages = $this->container->get('ezpublish.config.resolver')->getParameter('languages');
+
+        $repository = $this->container->get( 'ezpublish.api.repository' );
+        $contentService = $repository->getContentService();
+        $locationService = $repository->getLocationService();
+        $contentTypeService = $repository->getContentTypeService();
+
+        $contentType = $contentTypeService->loadContentTypeByIdentifier('image');
+
+        $createdContent = [];
+
+        $repository->beginTransaction();
+        /** @var UploadedFile $file */
+        foreach ($files as $file) {
+            try {
+                $contentCreateStruct = $contentService->newContentCreateStruct($contentType, $languages[0]);
+
+                $contentCreateStruct->setField('name', $file->getClientOriginalName());
+                $contentCreateStruct->setField('image', $file->getRealPath());
+
+                $locationCreateStruct = $locationService->newLocationCreateStruct(51);
+                $draft = $contentService->createContent($contentCreateStruct, array($locationCreateStruct));
+                $createdContent[] = $contentService->publishVersion($draft->versionInfo);
+            } catch (\Exception $e) {
+                $repository->rollback();
+            }
+        }
+
+        $repository->commit();
+
+        return $createdContent;
+    }
+
+    public function uploadAndGenerate(Request $request)
+    {
+        if ($request->files->count() > 10) {
+            return $this->render(
+                'NetgenRemoteMediaBundle:ngadminui/plugin/dashboard:multiupload.html.twig',
+                [
+                    'error' => 'Too many files. Please select under 10 files'
+                ]
+            );
+        }
+
+        if ($request->getMethod() === 'POST') {
+            $contentObjects = $this->handleFiles($request->files->get('file'));
+
+            // @todo: redirect
+            return $this->render(
+                'NetgenRemoteMediaBundle:ngadminui/plugin/dashboard:multiupload_summary.html.twig',
+                [
+                    'content_objects' => $contentObjects
+                ]
+            );
+        }
+
+        // we need to display multi-upload
+        // each uploaded image will automatically generate new ez content, with title set from the image and image itself set in remote media field
+        // we'll need to limit to max 10 objects probably due to the memory/timeout limits
+
+        // OPTIONAL: configure a way to quickly fill out one more field (description, caption, or something similar)
+        return $this->render(
+            'NetgenRemoteMediaBundle:ngadminui/plugin/dashboard:multiupload.html.twig'
+        );
     }
 }
